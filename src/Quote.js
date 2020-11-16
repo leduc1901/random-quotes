56e1f42639eb3c2c0cdaa6dd7491b79526e7940a
Kyle <letrongduc1999@gmail.com>
2020-11-16 11:52:23 +0700

    first commit

---
+++ src/Quote.js
@@ -0,0 +1,12 @@
+import { useSpring, animated } from 'react-spring';
+
+export default function Quotes(props){
+
+    const { opacity } = useSpring({
+        opacity: props.isLoading ? 0 : 1
+    })
+
+    return (
+        <animated.div className="text" style={{ opacity:opacity }} >"{props.text.quoteText}"</animated.div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState , useEffect } from 'react'
+import { useSpring, animated } from 'react-spring';
+import { render } from 'react-dom'
+import axios from 'axios'
+import Quote from './Quote'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function App(){
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [stateAuthor , setStateAuthor] = useState(false)
+  const [author , setAuthor] = useState(null)
+  const [quotes, setQuotes] = useState([])
+  const [genre, setGenre] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const [roll, setRoll] = useState(false)
+
+  useEffect(() => {
+    getQuotes()
+  }, [])
+
+  const {opacity, top , absolute, transform, subTop} = useSpring({
+    opacity: isLoading ? 0.5 : 1,
+    top: stateAuthor ? '120px' : '0px',
+    absolute: stateAuthor ? 'absolute' : '',
+    transform: `rotate(${roll ? 360 : 0}deg)`
+  })
+
+  async function getQuotes(){
+    try {
+      setLoading(true)
+      setStateAuthor(false)
+      const quotes = await axios.get('https://quote-garden.herokuapp.com/api/v2/quotes/random').then(res => {
+        setQuotes([res.data.quote])
+        setAuthor(res.data.quote.quoteAuthor)
+        setGenre(res.data.quote.quoteGenre)
+        setLoading(false)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function getAuthor(author){
+    try {
+      setStateAuthor(true)
+      setLoading(true)
+      const quotes = await axios.get(`https://quote-garden.herokuapp.com/api/v2/authors/${author}?page=1&limit=10`).then(res => {
+        console.log(res)
+        setGenre('quotes')
+        setQuotes(res.data.quotes)
+        setLoading(false)
+      })
+    } catch (error) {
+      
+    }
+  }
+
+  return (
+    <>
+    <div className="random-btn" onClick={() => getQuotes()}>Random
+    <animated.i class="fas fa-redo-alt" onMouseEnter={() => setRoll(true)} onMouseLeave={() => setRoll(false)} style={{ transform: transform}} ></animated.i>
+    </div>
+      <div className="container">
+        <animated.div className="quotes" style={{ top: top, position: absolute}}>
+          {quotes.map((item , i) => {
+            return (
+              <Quote text={item} isLoading={isLoading}/>
+            )
+          })}
+        </animated.div>
+        <animated.div className="info" onClick={() => getAuthor(author)} style={{opacity: opacity, position: absolute}}>
+          <div className="author" >{author}</div>
+          <div className="genre">{genre}</div>
+        </animated.div>
+      </div>
+    </>
+  )
+}
+
+render(<App />, document.getElementById('root'))
